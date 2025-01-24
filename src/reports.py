f83261f9c781f5107c5870f15410ba41195bca1d
Sergey Korobeynikov <skoro@smn35.com>
2025-01-24 12:15:52 +0300

    add some impotrs in in src/reports.py module decorator @save_to_file() applied to get_report_by_category func in src/reports.py module date parsing format changed in get_report_by_category func in src/reports.py module output format in get_report_by_category func in src/reports.py module changed to JSON

--- src/reports.py
+++ src/reports.py
@@ -1,3 +1,4 @@
+import os
 from datetime import datetime
 from typing import Optional
 
@@ -5,9 +6,11 @@ import pandas as pd
 from dateutil.relativedelta import relativedelta
 from pandas import DataFrame
 
+from config import ROOT_PATH
+from src.decorators import save_to_file
 from src.utils import filter_dataframe, get_data
 
-
+@save_to_file()
 def get_report_by_category(data_path: str, category: str, optional_date: Optional[str] = None) -> DataFrame:
     """Функция возвращает траты по заданной категории за последние 3 месяца (от переданной даты).
      Args:
@@ -19,15 +22,27 @@ def get_report_by_category(data_path: str, category: str, optional_date: Optiona
     if optional_date is None:
         date = datetime.now()
     else:
-        date = datetime.strptime(optional_date, "%d.%m.%Y")
+        date = datetime.strptime(optional_date, "%Y-%m-%d %H:%M:%S")
     old_date = date - relativedelta(months=3)
     df = get_data(data_path)
     df["Дата операции"] = pd.to_datetime(df["Дата операции"], format="%d.%m.%Y %H:%M:%S", errors="coerce")
     df = df[(df["Дата операции"] >= old_date) & (df["Дата операции"] <= date)]
     filter_conditions = {"Категория": category}
-    return filter_dataframe(df, filter_conditions)
-
+    result = filter_dataframe(df, filter_conditions)
+    return result.to_json(orient='records', force_ascii=False)
 
 if __name__ == "__main__":
-    input_date = input('Введите дату в формате ДД.ММ.ГГГГ. В базе данные с 01.01.2018 по 31.12.2021')
-    print(get_report_by_category("data/operations.xlsx", "Супермаркеты", input_date))
+    while True:
+        try:
+            date: Optional[str] = input(
+                "Введите строку с датой и временем в формате YYYY-MM-DD HH:MM:SS"
+                " в диапазоне с 2018-01-01 по 2021-12-31,"
+                " или нажмите Enter для использования текущей даты: "
+            ).strip()
+            if not date:  # Пустой ввод — текущая дата
+                date = None
+            data_path = os.path.join(ROOT_PATH, 'data', 'operations.xlsx')
+            print(get_report_by_category(data_path, "Супермаркеты",date))
+            break
+        except ValueError as e:
+            print(e)
