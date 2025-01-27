0cbdbddf8e50e8223da23ef6d7d79fe6940bf35e
Sergey Korobeynikov <skoro@smn35.com>
2025-01-27 18:46:26 +0300

    completely refactored module src/services.py

--- src/services.py
+++ src/services.py
@@ -2,24 +2,27 @@ import os.path
 import re
 from typing import Optional
 
-from pandas import DataFrame
-
+from config import ROOT_PATH
 from src.decorators import save_to_file
 from src.utils import filter_dataframe, get_data
-from config import ROOT_PATH
+
 
 @save_to_file()
 def find_money_transfers_from_individuals(data_path: str) -> str:
-    """Функция, возвращающая DataFrame с переводами только физическим лицам.
+    """Функция, возвращающая JSON с переводами только физическим лицам.
      Args:
         data_path (str): путь к исходному DataFrame.
     Returns:
-        DataFrame: Отфильтрованный DataFrame."""
+        str: Отфильтрованный JSON."""
     pattern = re.compile(r"^\s*[A-ZА-ЯЁ]{1}[a-zа-яё]+\s+[A-ZА-ЯЁ]{1}\.\s*$")
-    df = get_data(data_path)
+    try:
+        df = get_data(data_path)
+    except FileNotFoundError:
+        raise FileNotFoundError("Файл не найден")
+
     filter_conditions = {"Категория": "Переводы", "Описание": pattern}
     result = filter_dataframe(df, filter_conditions, "AND")
-    return result.to_json(orient='records', force_ascii=False)
+    return result.to_json(orient="records", force_ascii=False)
 
 
 if __name__ == "__main__":
@@ -32,9 +35,8 @@ if __name__ == "__main__":
             ).strip()
             if not date:  # Пустой ввод — текущая дата
                 date = None
-            path = os.path.join(ROOT_PATH, 'data', 'operations.xlsx')
+            path = os.path.join(ROOT_PATH, "data", "operations.xlsx")
             print(find_money_transfers_from_individuals(path))
             break
         except ValueError as e:
             print(e)
-
