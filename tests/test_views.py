d2a42f17be884d518e97f29b19b2371c4dbf0f4d
Sergey Korobeynikov <skoro@smn35.com>
2025-01-29 13:51:24 +0300

    tests/test_views.py formatted

--- tests/test_views.py
+++ tests/test_views.py
@@ -2,13 +2,16 @@ from datetime import datetime
 
 import pandas as pd
 import pytest
+from pandas import DataFrame
+
+
 from pandas.testing import assert_frame_equal
 
-from src.views import get_operations_for_current_month, sum_by_category, get_total_spending, get_top_5
+from src.views import get_operations_for_current_month, get_top_5, get_total_spending, sum_by_category
 
 
 @pytest.fixture
-def sample_dataframe():
+def sample_dataframe() -> DataFrame:
     """Фикстура для создания тестового DataFrame с транзакциями."""
     data = {
         "Дата операции": ["2025-01-10", "2025-01-20", "2025-02-05", "2025-01-25"],
@@ -19,7 +22,7 @@ def sample_dataframe():
     return df
 
 
-def test_get_operations_current_month_default_date(sample_dataframe):
+def test_get_operations_current_month_default_date(sample_dataframe: DataFrame) -> None:
     """Тест: фильтрация транзакций за текущий месяц (текущая дата по умолчанию)."""
     current_date = "2025-01-27 00:00:00"
     filtered_df = get_operations_for_current_month(sample_dataframe, current_date)
@@ -34,7 +37,7 @@ def test_get_operations_current_month_default_date(sample_dataframe):
     assert_frame_equal(filtered_df.reset_index(drop=True), expected_df)
 
 
-def test_get_operations_current_month_string_date(sample_dataframe):
+def test_get_operations_current_month_string_date(sample_dataframe: DataFrame) -> None:
     """Тест: фильтрация транзакций за текущий месяц (строка в формате даты)."""
     current_date = "2025-01-27 00:00:00"
     filtered_df = get_operations_for_current_month(sample_dataframe, current_date)
@@ -47,7 +50,7 @@ def test_get_operations_current_month_string_date(sample_dataframe):
     assert_frame_equal(filtered_df.reset_index(drop=True), expected_df)
 
 
-def test_get_operations_current_month_datetime_date(sample_dataframe):
+def test_get_operations_current_month_datetime_date(sample_dataframe: DataFrame) -> None:
     """Тест: фильтрация транзакций за текущий месяц (объект datetime)."""
     current_date = datetime(2025, 1, 15, 00, 00, 00)
     filtered_df = get_operations_for_current_month(sample_dataframe, current_date)
@@ -61,7 +64,7 @@ def test_get_operations_current_month_datetime_date(sample_dataframe):
     assert_frame_equal(filtered_df.reset_index(drop=True), expected_df)
 
 
-def test_get_operations_current_month_cutoff_date(sample_dataframe):
+def test_get_operations_current_month_cutoff_date(sample_dataframe: DataFrame) -> None:
     """Тест: отсечение транзакций, произошедших после переданной даты."""
     current_date = datetime(2025, 1, 20, 00, 00, 00)
     filtered_df = get_operations_for_current_month(sample_dataframe, current_date)
@@ -76,7 +79,7 @@ def test_get_operations_current_month_cutoff_date(sample_dataframe):
     assert_frame_equal(filtered_df.reset_index(drop=True), expected_df)
 
 
-def test_get_operations_current_month_no_current_date(sample_dataframe):
+def test_get_operations_current_month_no_current_date(sample_dataframe: DataFrame) -> None:
     """Тест: передача аргумента current_date=None."""
     filtered_df = get_operations_for_current_month(sample_dataframe, None)
     # Проверяем, что фильтр отработал для текущего месяца (текущая дата - дата выполнения теста).
@@ -85,25 +88,25 @@ def test_get_operations_current_month_no_current_date(sample_dataframe):
     current_year = current_date.year
 
     expected_df = sample_dataframe[
-        (pd.to_datetime(sample_dataframe["Дата операции"]).dt.year == current_year) &
-        (pd.to_datetime(sample_dataframe["Дата операции"]).dt.month == current_month)
-        ]
+        (pd.to_datetime(sample_dataframe["Дата операции"]).dt.year == current_year)
+        & (pd.to_datetime(sample_dataframe["Дата операции"]).dt.month == current_month)
+    ]
     assert_frame_equal(filtered_df.reset_index(drop=True), expected_df.reset_index(drop=True))
 
 
-def test_invalid_date_format(sample_dataframe):
+def test_invalid_date_format(sample_dataframe: DataFrame) -> None:
     """Тест: передача некорректной даты (строка)."""
     with pytest.raises(ValueError, match="Передана некорректная дата: invalid-date"):
         get_operations_for_current_month(sample_dataframe, "invalid-date")
 
 
-def test_invalid_date_type(sample_dataframe):
+def test_invalid_date_type(sample_dataframe: DataFrame) -> None:
     """Тест: передача недопустимого типа данных в current_date."""
     with pytest.raises(ValueError, match="Аргумент .* должен быть строкой .* объектом datetime или None"):
         get_operations_for_current_month(sample_dataframe, 123)
 
 
-def test_filter_empty_dataframe():
+def test_filter_empty_dataframe() -> None:
     """Тест: передача пустого DataFrame."""
     df = pd.DataFrame(columns=["Дата операции", "Сумма"])
     filtered_df = get_operations_for_current_month(df, "2025-01-27 00:00:00")
@@ -112,27 +115,34 @@ def test_filter_empty_dataframe():
 
 def test_sum_by_category_succes() -> None:
     """Тест успешной группировки по категориям"""
-    data = {'Категория': ['первая', 'вторая', 'третья', 'первая'], 'Сумма операции с округлением': [100, 200, 300, 400]}
+    data = {
+        "Категория": ["первая", "вторая", "третья", "первая"],
+        "Сумма операции с округлением": [100, 200, 300, 400],
+    }
     df = pd.DataFrame(data)
-
     result = sum_by_category(df)
-    expected_data = {'Категория': ['вторая', 'первая', 'третья'], 'Сумма операции с округлением': [200, 500, 300]}
+
+    expected_data = {"Категория": ["вторая", "первая", "третья"], "Сумма операции с округлением": [200, 500, 300]}
     expected_result = pd.DataFrame(expected_data)
+
     assert_frame_equal(result.reset_index(drop=True), expected_result.reset_index(drop=True))
 
 
 def test_get_total_spending() -> None:
     """Тест успешной группировки по номерам карт"""
     data = {
-        'Номер карты': ['1111', '2222', '4444', '2222'],
-        'Сумма операции с округлением': [100, 200, 300, 400],
-        "Кэшбэк": [1, 2, 4, 5]}
+        "Номер карты": ["1111", "2222", "4444", "2222"],
+        "Сумма операции с округлением": [100, 200, 300, 400],
+        "Кэшбэк": [1, 2, 4, 5],
+    }
     df = pd.DataFrame(data)
 
     result = get_total_spending(df)
-    expected_data = {'Номер карты': ['1111', '2222', '4444'],
-                     'Сумма операции с округлением': [100, 600, 300],
-                     "Кэшбэк": [1, 7, 4]}
+    expected_data = {
+        "Номер карты": ["1111", "2222", "4444"],
+        "Сумма операции с округлением": [100, 600, 300],
+        "Кэшбэк": [1, 7, 4],
+    }
     expected_result = pd.DataFrame(expected_data)
     assert_frame_equal(result.reset_index(drop=True), expected_result.reset_index(drop=True))
 
@@ -140,19 +150,27 @@ def test_get_total_spending() -> None:
 def test_get_top_5() -> None:
     """Тест фильтрации Топ-5 транзакций"""
     data = {
-        "Дата операции": ['2021-25-06', '2021-24-06', '2021-20-06', '2020-25-06', '2018-25-06', '2020-25-08', '2019-25-08'],
+        "Дата операции": [
+            "2021-25-06",
+            "2021-24-06",
+            "2021-20-06",
+            "2020-25-06",
+            "2018-25-06",
+            "2020-25-08",
+            "2019-25-08",
+        ],
         "Сумма операции с округлением": [1600, 16000, 20, 50, 100, 1000, 47],
-        "Категория": ['1', '2', '3', '4', '5', '6', '7'],
-        "Описание": ['Колхоз', 'Ozon.ru', 'Константин Л.', 'Константин Л.', 'Ситидрайв', 'РЖД', 'Mouse Tail'
-                     ]}
+        "Категория": ["1", "2", "3", "4", "5", "6", "7"],
+        "Описание": ["Колхоз", "Ozon.ru", "Константин Л.", "Константин Л.", "Ситидрайв", "РЖД", "Mouse Tail"],
+    }
     df = pd.DataFrame(data)
     result = get_top_5(df)
 
     expected_data = {
-        "Дата операции": ['2021-24-06', '2021-25-06', '2020-25-08', '2018-25-06', '2020-25-06'],
+        "Дата операции": ["2021-24-06", "2021-25-06", "2020-25-08", "2018-25-06", "2020-25-06"],
         "Сумма операции с округлением": [16000, 1600, 1000, 100, 50],
-        "Категория": ['2', '1', '6', '5', '4'],
-        "Описание": ['Ozon.ru', 'Колхоз', 'РЖД', 'Ситидрайв', 'Константин Л.'
-                     ]}
+        "Категория": ["2", "1", "6", "5", "4"],
+        "Описание": ["Ozon.ru", "Колхоз", "РЖД", "Ситидрайв", "Константин Л."],
+    }
     expected_result = pd.DataFrame(expected_data)
     assert_frame_equal(result.reset_index(drop=True), expected_result.reset_index(drop=True))
