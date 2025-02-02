4112ff71018888830db294ff1542e158668259e5
Sergey Korobeynikov <skoro@smn35.com>
2025-02-02 13:03:56 +0300

    logging added in src/reports.py

--- src/reports.py
+++ src/reports.py
@@ -1,6 +1,8 @@
+import logging
 import os
 from datetime import datetime
-from typing import Optional, Any
+from logging import getLogger, Formatter, DEBUG, WARNING
+from typing import Any, Union
 
 import pandas as pd
 from dateutil.relativedelta import relativedelta
@@ -8,49 +10,64 @@ from pandas import DataFrame
 
 from config import ROOT_PATH
 from src.decorators import save_to_file
-from src.utils import filter_dataframe, get_data
+from src.utils import filter_dataframe, get_data, get_date
 
 
 @save_to_file()
-def get_report_by_category(data_path: str, category: str, optional_date: Optional[str] = None) -> Any:
-    """Функция возвращает траты по заданной категории за последние 3 месяца (от переданной даты).
-     Args:
-        data_path (str): путь к исходному DataFrame.
-        category (str): категория, по которой нужно отфильтровать DataFrame.
-        optional_date (str): опциональная дата. Если дата не передана, то берется текущая дата.
-    Returns:
-        DataFrame: Отфильтрованный DataFrame."""
+def get_report_by_category() -> str:
+    """Возвращает JSON с тратами по категории за последние 3 месяца."""
+
+    loger = getLogger('reports')
+
+    if not loger.handlers:  # Избегаем дублирования хендлеров
+        formatter = Formatter("%(asctime)s - %(levelname)s - %(name)s - %(message)s")
+        consolehandler = logging.StreamHandler()
+        consolehandler.setFormatter(formatter)
+        consolehandler.setLevel(DEBUG)
+
+        logpath = os.path.join(ROOT_PATH, 'logs')
+        os.makedirs(logpath, exist_ok=True)
+        logfile = os.path.join(logpath, 'log.txt')
+
+        try:
+            filehandler = logging.FileHandler(logfile, mode="a")
+            filehandler.setFormatter(formatter)
+            filehandler.setLevel(WARNING)
+            loger.addHandler(consolehandler)
+            loger.addHandler(filehandler)
+        except PermissionError as e:
+            loger.error(f"Ошибка доступа к файлу логов: {e}")
+
+    optional_date = get_date()
     if optional_date is None:
         date = datetime.now()
+        loger.warning('Дата не передана, используется текущая дата')
     else:
         date = datetime.strptime(optional_date, "%Y-%m-%d %H:%M:%S")
+        loger.debug(f'Дата передана: {date}')
+
     old_date = date - relativedelta(months=3)
+    data_path = os.path.join(ROOT_PATH, 'data', 'operations.xlsx')
+
     df = get_data(data_path)
+    loger.debug('Данные из файла получены')
+
     df["Дата операции временная"] = pd.to_datetime(df["Дата операции"], format="%d.%m.%Y %H:%M:%S", errors="coerce")
 
-    # Фильтруем DataFrame по дате за последние 3 месяца
+    # Фильтрация по дате
     df = df[(df["Дата операции временная"] >= old_date) & (df["Дата операции временная"] <= date)]
     df = df.drop(columns=["Дата операции временная"])
+    loger.debug(f'Данные отфильтрованы по дате:\n{df["Дата операции"]}')
 
-    # Фильтруем DataFrame по переданной категории
+    # Фильтрация по категории
+    category = "Супермаркеты"
     filter_conditions = {"Категория": category}
     result = filter_dataframe(df, filter_conditions)
+    loger.debug(f'Данные отфильтрованы по категории:\n{result["Категория"]}')
 
     return result.to_json(orient="records", force_ascii=False)
 
 
-# if __name__ == "__main__":
-#     while True:
-#         try:
-#             date: Optional[str] = input(
-#                 "Введите строку с датой и временем в формате YYYY-MM-DD HH:MM:SS"
-#                 " в диапазоне с 2018-01-01 по 2021-12-31,"
-#                 " или нажмите Enter для использования текущей даты: "
-#             ).strip()
-#             if not date:  # Пустой ввод — текущая дата
-#                 date = None
-#             data_path = os.path.join(ROOT_PATH, 'data', 'operations.xlsx')
-#             print(get_report_by_category(data_path, "Супермаркеты",date))
-#             break
-#         except ValueError as e:
-#             print(e)
+if __name__ == "__main__":
+    getLogger('reports').setLevel(DEBUG)  # Логируем только в main
+    print(get_report_by_category())
