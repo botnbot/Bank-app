1a99a6ce1ed1c287871a4a716853bca783af2543
Sergey Korobeynikov <skoro@smn35.com>
2025-02-02 21:37:28 +0300

    some fix in src/services.py

--- src/services.py
+++ src/services.py
@@ -1,15 +1,12 @@
 import logging
 import os.path
 import re
-from logging import getLogger, Formatter, DEBUG, WARNING
-from typing import Optional, Any, Union
+from logging import DEBUG, WARNING, Formatter, getLogger
+from typing import Any, Union
 
 from config import ROOT_PATH
 from src.decorators import save_to_file
 from src.utils import filter_dataframe, get_data
-
-
-
 
 
 @save_to_file()
@@ -20,7 +17,7 @@ def find_money_transfers_from_individuals() -> Union[str, Any]:
     Returns:
         str: Отфильтрованный JSON."""
 
-    loger = getLogger('reports')
+    loger = getLogger("reports")
     formatter = Formatter("%(asctime)s - %(levelname)s - %(name)s - %(message)s")
     loger.setLevel(DEBUG)
 
@@ -28,9 +25,9 @@ def find_money_transfers_from_individuals() -> Union[str, Any]:
     consolehandler.setFormatter(formatter)
     consolehandler.setLevel(DEBUG)
 
-    logpath = os.path.join(ROOT_PATH, 'logs')
+    logpath = os.path.join(ROOT_PATH, "logs")
     os.makedirs(logpath, exist_ok=True)
-    logfile = os.path.join(ROOT_PATH, 'logs', 'log.txt')
+    logfile = os.path.join(ROOT_PATH, "logs", "log.txt")
 
     try:
         filehandler = logging.FileHandler(logfile, mode="a")
@@ -42,16 +39,14 @@ def find_money_transfers_from_individuals() -> Union[str, Any]:
     except PermissionError as e:
         loger.error(f"Ошибка доступа к файлу логов: {e}")
 
-
     pattern = re.compile(r"^\s*[A-ZА-ЯЁ]{1}[a-zа-яё]+\s+[A-ZА-ЯЁ]{1}\.\s*$")
     try:
-        data_path = os.path.join(ROOT_PATH, 'data', 'operations.xlsx')
+        data_path = os.path.join(ROOT_PATH, "data", "operations.xlsx")
         df = get_data(data_path)
         loger.debug("Данные из файла загружены")
     except FileNotFoundError as e:
         loger.error(f"Ошибка доступа к файлу с данными: {e}")
-        raise FileNotFoundError("Файл не с данными найден")
-
+        raise FileNotFoundError("Файл с данными не найден")
 
     filter_conditions = {"Категория": "Переводы", "Описание": pattern}
     result = filter_dataframe(df, filter_conditions, "AND")
@@ -60,19 +55,5 @@ def find_money_transfers_from_individuals() -> Union[str, Any]:
 
 
 if __name__ == "__main__":
-    print(find_money_transfers_from_individuals())
+    find_money_transfers_from_individuals()
 
-#     while True:
-#         try:
-#             date: Optional[str] = input(
-#                 "Введите строку с датой и временем в формате YYYY-MM-DD HH:MM:SS"
-#                 " в диапазоне с 2018-01-01 по 2021-12-31,"
-#                 " или нажмите Enter для использования текущей даты: "
-#             ).strip()
-#             if not date:  # Пустой ввод — текущая дата
-#                 date = None
-#             path = os.path.join(ROOT_PATH, "data", "operations.xlsx")
-#             print(find_money_transfers_from_individuals(path))
-#             break
-#         except ValueError as e:
-#             print(e)
