c370c5b7fbbf2f49d9cfd9220d6975e2b8908913
Sergey Korobeynikov <skoro@smn35.com>
2025-02-11 19:11:14 +0300

    imports in src/services.py optimized

--- src/services.py
+++ src/services.py
@@ -1,8 +1,6 @@
-import os.path
 import re
-from typing import Optional, Any
+from typing import Any
 
-from config import ROOT_PATH
 from src.decorators import save_to_file
 from src.utils import filter_dataframe, get_data
 
@@ -23,20 +21,3 @@ def find_money_transfers_from_individuals(data_path: str) -> Any:
     filter_conditions = {"Категория": "Переводы", "Описание": pattern}
     result = filter_dataframe(df, filter_conditions, "AND")
     return result.to_json(orient="records", force_ascii=False)
-
-
-# if __name__ == "__main__":
-#     while True:
-#         try:
-#             date: Optional[str] = input(
-#                 "Введите строку с датой и временем в формате YYYY-MM-DD HH:MM:SS"
-#                 " в диапазоне с 2018-01-01 по 2021-12-31,"
-#                 " или нажмите Enter для использования текущей даты: "
-#             ).strip()
-#             if not date:  # Пустой ввод — текущая дата
-#                 date = None
-#             path = os.path.join(ROOT_PATH, "data", "operations.xlsx")
-#             print(find_money_transfers_from_individuals(path))
-#             break
-#         except ValueError as e:
-#             print(e)
