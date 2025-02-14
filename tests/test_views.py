57a8bf502bcf06ce658b8ff26be9e7959b8bf159
Sergey Korobeynikov <skoro@smn35.com>
2025-02-14 20:28:47 +0300

    disable_logging fixture added &  tests for get_operations_for_current_month deleted &  tests for views() func changed

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,6 +1,6 @@
 import json
 import logging
-from datetime import datetime
+from typing import Generator
 from unittest.mock import Mock, patch
 
 import pandas as pd
@@ -8,7 +8,15 @@ import pytest
 from pandas import DataFrame
 from pandas.testing import assert_frame_equal
 
-from src.views import get_operations_for_current_month, get_top_5, get_total_spending, main, sum_by_category
+from src.views import get_top_5, get_total_spending, sum_by_category, views
+
+
+@pytest.fixture(scope="function", autouse=True)
+def disable_logging() -> Generator:
+    """Отключает логирование на время тестов."""
+    logging.disable(logging.CRITICAL)
+    yield
+    logging.disable(logging.NOTSET)
 
 
 @pytest.fixture
@@ -23,96 +31,95 @@ def sample_dataframe() -> DataFrame:
     return df
 
 
-
-def test_get_operations_current_month_default_date(sample_dataframe: DataFrame) -> None:
-    """Тест: фильтрация транзакций за текущий месяц (текущая дата по умолчанию)."""
-    current_date = "2025-01-27 00:00:00"
-    filtered_df = get_operations_for_current_month(sample_dataframe, current_date)
-
-    expected_data = {
-        "Дата операции": ["2025-01-10", "2025-01-20", "2025-01-25"],
-        "Сумма": [100, 200, 400],
-    }
-    expected_df = pd.DataFrame(expected_data)
-    expected_df["Дата операции"] = pd.to_datetime(expected_df["Дата операции"])
-
-    assert_frame_equal(filtered_df.reset_index(drop=True), expected_df)
-
-
-def test_get_operations_current_month_string_date(sample_dataframe: DataFrame) -> None:
-    """Тест: фильтрация транзакций за текущий месяц (строка в формате даты)."""
-    current_date = "2025-01-27 00:00:00"
-    filtered_df = get_operations_for_current_month(sample_dataframe, current_date)
-    expected_data = {
-        "Дата операции": ["2025-01-10", "2025-01-20", "2025-01-25"],
-        "Сумма": [100, 200, 400],
-    }
-    expected_df = pd.DataFrame(expected_data)
-    expected_df["Дата операции"] = pd.to_datetime(expected_df["Дата операции"])
-    assert_frame_equal(filtered_df.reset_index(drop=True), expected_df)
-
-
-def test_get_operations_current_month_datetime_date(sample_dataframe: DataFrame) -> None:
-    """Тест: фильтрация транзакций за текущий месяц (объект datetime)."""
-    current_date = datetime(2025, 1, 15, 00, 00, 00)
-    filtered_df = get_operations_for_current_month(sample_dataframe, current_date)
-    expected_data = {
-        "Дата операции": ["2025-01-10"],
-        "Сумма": [100],
-    }
-    expected_df = pd.DataFrame(expected_data)
-    expected_df["Дата операции"] = pd.to_datetime(expected_df["Дата операции"])
-
-    assert_frame_equal(filtered_df.reset_index(drop=True), expected_df)
-
-
-def test_get_operations_current_month_cutoff_date(sample_dataframe: DataFrame) -> None:
-    """Тест: отсечение транзакций, произошедших после переданной даты."""
-    current_date = datetime(2025, 1, 20, 00, 00, 00)
-    filtered_df = get_operations_for_current_month(sample_dataframe, current_date)
-
-    expected_data = {
-        "Дата операции": ["2025-01-10", "2025-01-20"],
-        "Сумма": [100, 200],
-    }
-    expected_df = pd.DataFrame(expected_data)
-    expected_df["Дата операции"] = pd.to_datetime(expected_df["Дата операции"])
-
-    assert_frame_equal(filtered_df.reset_index(drop=True), expected_df)
-
-
-def test_get_operations_current_month_no_current_date(sample_dataframe: DataFrame) -> None:
-    """Тест: передача аргумента current_date=None."""
-    filtered_df = get_operations_for_current_month(sample_dataframe, None)
-    # Проверяем, что фильтр отработал для текущего месяца (текущая дата - дата выполнения теста).
-    current_date = datetime.now()
-    current_month = current_date.month
-    current_year = current_date.year
-
-    expected_df = sample_dataframe[
-        (pd.to_datetime(sample_dataframe["Дата операции"]).dt.year == current_year)
-        & (pd.to_datetime(sample_dataframe["Дата операции"]).dt.month == current_month)
-    ]
-    assert_frame_equal(filtered_df.reset_index(drop=True), expected_df.reset_index(drop=True))
-
-
-def test_invalid_date_format(sample_dataframe: DataFrame) -> None:
-    """Тест: передача некорректной даты (строка)."""
-    with pytest.raises(ValueError, match="Передана некорректная дата: invalid-date"):
-        get_operations_for_current_month(sample_dataframe, "invalid-date")
-
-
-def test_invalid_date_type(sample_dataframe: DataFrame) -> None:
-    """Тест: передача недопустимого типа данных в current_date."""
-    with pytest.raises(ValueError, match="Аргумент .* должен быть строкой .* объектом datetime или None"):
-        get_operations_for_current_month(sample_dataframe, 123)
-
-
-def test_filter_empty_dataframe() -> None:
-    """Тест: передача пустого DataFrame."""
-    df = pd.DataFrame(columns=["Дата операции", "Сумма"])
-    filtered_df = get_operations_for_current_month(df, "2025-01-27 00:00:00")
-    assert filtered_df.empty
+# def test_get_operations_current_month_default_date(sample_dataframe: DataFrame) -> None:
+#     """Тест: фильтрация транзакций за текущий месяц (текущая дата по умолчанию)."""
+#     current_date = "2025-01-27 00:00:00"
+#     filtered_df = get_operations_for_current_month(sample_dataframe, current_date)
+#
+#     expected_data = {
+#         "Дата операции": ["2025-01-10", "2025-01-20", "2025-01-25"],
+#         "Сумма": [100, 200, 400],
+#     }
+#     expected_df = pd.DataFrame(expected_data)
+#     expected_df["Дата операции"] = pd.to_datetime(expected_df["Дата операции"])
+#
+#     assert_frame_equal(filtered_df.reset_index(drop=True), expected_df)
+#
+#
+# def test_get_operations_current_month_string_date(sample_dataframe: DataFrame) -> None:
+#     """Тест: фильтрация транзакций за текущий месяц (строка в формате даты)."""
+#     current_date = "2025-01-27 00:00:00"
+#     filtered_df = get_operations_for_current_month(sample_dataframe, current_date)
+#     expected_data = {
+#         "Дата операции": ["2025-01-10", "2025-01-20", "2025-01-25"],
+#         "Сумма": [100, 200, 400],
+#     }
+#     expected_df = pd.DataFrame(expected_data)
+#     expected_df["Дата операции"] = pd.to_datetime(expected_df["Дата операции"])
+#     assert_frame_equal(filtered_df.reset_index(drop=True), expected_df)
+#
+#
+# def test_get_operations_current_month_datetime_date(sample_dataframe: DataFrame) -> None:
+#     """Тест: фильтрация транзакций за текущий месяц (объект datetime)."""
+#     current_date = datetime(2025, 1, 15, 00, 00, 00)
+#     filtered_df = get_operations_for_current_month(sample_dataframe, current_date)
+#     expected_data = {
+#         "Дата операции": ["2025-01-10"],
+#         "Сумма": [100],
+#     }
+#     expected_df = pd.DataFrame(expected_data)
+#     expected_df["Дата операции"] = pd.to_datetime(expected_df["Дата операции"])
+#
+#     assert_frame_equal(filtered_df.reset_index(drop=True), expected_df)
+#
+#
+# def test_get_operations_current_month_cutoff_date(sample_dataframe: DataFrame) -> None:
+#     """Тест: отсечение транзакций, произошедших после переданной даты."""
+#     current_date = datetime(2025, 1, 20, 00, 00, 00)
+#     filtered_df = get_operations_for_current_month(sample_dataframe, current_date)
+#
+#     expected_data = {
+#         "Дата операции": ["2025-01-10", "2025-01-20"],
+#         "Сумма": [100, 200],
+#     }
+#     expected_df = pd.DataFrame(expected_data)
+#     expected_df["Дата операции"] = pd.to_datetime(expected_df["Дата операции"])
+#
+#     assert_frame_equal(filtered_df.reset_index(drop=True), expected_df)
+#
+#
+# def test_get_operations_current_month_no_current_date(sample_dataframe: DataFrame) -> None:
+#     """Тест: передача аргумента current_date=None."""
+#     filtered_df = get_operations_for_current_month(sample_dataframe, None)
+#     # Проверяем, что фильтр отработал для текущего месяца (текущая дата - дата выполнения теста).
+#     current_date = datetime.now()
+#     current_month = current_date.month
+#     current_year = current_date.year
+#
+#     expected_df = sample_dataframe[
+#         (pd.to_datetime(sample_dataframe["Дата операции"]).dt.year == current_year)
+#         & (pd.to_datetime(sample_dataframe["Дата операции"]).dt.month == current_month)
+#         ]
+#     assert_frame_equal(filtered_df.reset_index(drop=True), expected_df.reset_index(drop=True))
+#
+#
+# def test_invalid_date_format(sample_dataframe: DataFrame) -> None:
+#     """Тест: передача некорректной даты (строка)."""
+#     with pytest.raises(ValueError, match="Передана некорректная дата: invalid-date"):
+#         get_operations_for_current_month(sample_dataframe, "invalid-date")
+#
+#
+# def test_invalid_date_type(sample_dataframe: DataFrame) -> None:
+#     """Тест: передача недопустимого типа данных в current_date."""
+#     with pytest.raises(ValueError, match="Аргумент .* должен быть строкой .* объектом datetime или None"):
+#         get_operations_for_current_month(sample_dataframe, 123)
+#
+#
+# def test_filter_empty_dataframe() -> None:
+#     """Тест: передача пустого DataFrame."""
+#     df = pd.DataFrame(columns=["Дата операции", "Сумма"])
+#     filtered_df = get_operations_for_current_month(df, "2025-01-27 00:00:00")
+#     assert filtered_df.empty
 
 
 def test_sum_by_category_succes() -> None:
@@ -185,7 +192,6 @@ def test_get_top_5() -> None:
 @patch("src.views.get_stock_prices")
 @patch("src.views.get_exchange_rates")
 @patch("src.views.convert_to_rub")
-
 def test_views(
     mock_convert_to_rub: Mock,
     mock_get_exchange_rates: Mock,
@@ -196,56 +202,65 @@ def test_views(
     mock_get_data: Mock,
 ) -> None:
     """
-    Tестирует основную функцию main() и проверяет правильность JSON-ответа.
+    Tестирует основную функцию views() и проверяет правильность JSON-ответа.
     """
-    logging.disable(logging.CRITICAL)  # Отключает все логи
 
-    try:
-        df = pd.DataFrame(
-            {
-                "Дата операции": ["2024-01-10 12:00:00", "2024-01-15 15:30:00"],
-                "Номер карты": ["1234567890123456", "9876543210987654"],
-                "Сумма операции с округлением": [1000, 2000],
-                "Кэшбэк": [10, 20],
-                "Категория": ["Продукты", "Транспорт"],
-                "Описание": ["Покупка еды", "Проезд"],
-            }
-        )
-        # Передаем DataFrame вместо Mock
-        mock_get_data.return_value = df
-        mock_get_df_for_current_period.return_value = df
-        mock_get_total_spending.return_value = pd.DataFrame(
-            [{"Номер карты": "1234567890123456", "Сумма операции с округлением": 1000, "Кэшбэк": 10}]
-        )
+    df = pd.DataFrame(
+        {
+            "Дата операции": ["2024-01-10 12:00:00", "2024-01-15 15:30:00"],
+            "Номер карты": ["1234567890123456", "9876543210987654"],
+            "Сумма операции с округлением": [1000, 2000],
+            "Кэшбэк": [10, 20],
+            "Категория": ["Продукты", "Транспорт"],
+            "Описание": ["Покупка еды", "Проезд"],
+        }
+    )
+    # Передаем DataFrame вместо Mock
+    mock_get_data.return_value = df
+    mock_get_df_for_current_period.return_value = df
+    mock_get_total_spending.return_value = pd.DataFrame(
+        [{"Номер карты": "1234567890123456", "Сумма операции с округлением": 1000, "Кэшбэк": 10}]
+    )
 
-        mock_get_top_5.return_value = df.to_dict(orient="records")
+    mock_get_top_5.return_value = df.to_dict(orient="records")
 
-        mock_get_stock_prices.return_value = {"AAPL": 150.0}
-        mock_get_exchange_rates.return_value = {"USD": 80.0}
-        mock_convert_to_rub.return_value = {"USD": 80.0}
+    mock_get_stock_prices.return_value = {"AAPL": 150.0}
+    # mock_get_exchange_rates.return_value = {"USD": 80.0}
+    # mock_convert_to_rub.return_value = {"USD": 8.0}
 
-        response = main("2024-01-15 12:00:00")
-        result = json.loads(response)
-        print(result)
+    response = views("2024-01-15 12:00:00")
+    result = json.loads(response)
+    print(result)
 
-        # Проверяем, что результат - это JSON
-        assert isinstance(result, dict)
+    # Проверяем, что результат - это JSON
+    assert isinstance(result, dict)
 
-        assert "greeting" in result
-        assert "cards" in result
-        assert "top_transactions" in result
-        assert "currency_rates" in result
-        assert "stock_prices" in result
+    # Выводим результат для отладки
+    print(json.dumps(result, indent=4, ensure_ascii=False))
 
-        # Дополнительные проверки структуры JSON
-        assert isinstance(result["cards"], list)
-        assert isinstance(result["top_transactions"], list)
-        assert isinstance(result["currency_rates"], list)
-        assert isinstance(result["stock_prices"], list)
+    assert "greeting" in result
+    assert "cards" in result
+    assert "top_transactions" in result
+    assert "currency_rates" in result
+    assert "stock_prices" in result
 
-        # Проверка, что у карт есть все нужные поля
-        assert all("last_digits" in card for card in result["cards"])
-        assert all("total_spent" in card for card in result["cards"])
-        assert all("cashback" in card for card in result["cards"])
-    finally:
-        logging.disable(logging.NOTSET)  # Включает логирование обратно после теста
+    # Дополнительные проверки структуры JSON
+
+    assert isinstance(result["cards"], list)
+    assert isinstance(result["top_transactions"], list)
+    assert isinstance(result["currency_rates"], list)
+    if result["currency_rates"]:
+        assert (rate["currency"] == "USD" and rate["rate"] == 90 for rate in result["currency_rates"])
+    assert isinstance(result["stock_prices"], list)
+    if result["stock_prices"]:
+        assert result["stock_prices"][0]["stock"] == "AAPL"
+        assert result["stock_prices"][0]["price"] == 150
+
+    # Проверка, что у карт есть все нужные поля
+    assert all("last_digits" in card for card in result["cards"])
+    assert all("total_spent" in card for card in result["cards"])
+    assert all("cashback" in card for card in result["cards"])
+
+    assert result["cards"][0]["last_digits"] == "3456"
+    assert result["cards"][0]["total_spent"] == 1000
+    assert result["cards"][0]["cashback"] == 10
