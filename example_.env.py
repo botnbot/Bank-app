144bb773dc18bf4f4bd769207b35e9f9c5c49f35
Sergey Korobeynikov <skoro@smn35.com>
2025-02-17 13:56:23 +0300

    formatted example_.env.py

--- example_.env.py
+++ example_.env.py
@@ -1,5 +1,5 @@
 apikey = "вставьте реальный API_KEY"
-url = "finnhub.Client(apikey)"
+url = "finnhub.Client(apikey)"  # historical and live data for stocks
 
 API_KEY = "вставьте реальный API_KEY"
-url = "http://data.fixer.io/api/latest"
+url = "http://data.fixer.io/api/latest"  # Foreign exchange rates and currency conversion JSON API
